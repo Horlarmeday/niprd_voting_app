5dcc2781627733b91714e427a9ed54486c55870c
Mahmud <ajaomahmud@gmail.com>
2021-05-18 16:44:38 +0100

    feat: Add routes

---
+++ src/startup/routes.js
@@ -0,0 +1,29 @@
+import votersRoutes from '../modules/Voters/voters.routes';
+import votesRoutes from '../modules/Votes/votes.routes';
+
+export default server => {
+  server.use(votersRoutes);
+  server.use(votesRoutes);
+
+  server.use((req, res, next) => {
+    const apiTimeout = 18000;
+    // set the timeout for all HTTP requests
+    req.setTimeout(apiTimeout, () => {
+      const err = new Error('Request Timeout');
+      err.status = 408;
+      next(err);
+    });
+
+    // set the server response timeout for all HTTP requests
+    res.setTimeout(apiTimeout, () => {
+      const err = new Error('Service Unavailable');
+      err.status = 503;
+      next(err);
+    });
+    next();
+  });
+  server.use((req, res, next) => {
+    res.render('body/404');
+    // next(err);
+  });
+};
